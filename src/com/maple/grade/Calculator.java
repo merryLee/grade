dec72bd9006541e1e25cb0d8fecd648cfd048dc5
merryLee <ismpink@daum.net>
2015-01-06 22:59:35 +0900

    학점계산 기능 추가

--- src/com/maple/grade/Calculator.java
+++ src/com/maple/grade/Calculator.java
@@ -2,4 +2,14 @@ package com.maple.grade;
 
 public class Calculator {
 
+	public static String count(int num) {
+
+		if (num >= 90) return "A";
+		else if (num >= 80) return "B";
+		else if (num >= 70) return "C";
+		else if (num >= 60) return "D";
+		else return "F";
+
+	}
+
 }
--- src/com/maple/grade/Main.java
+++ src/com/maple/grade/Main.java
@@ -1,11 +1,21 @@
 package com.maple.grade;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
+
+		int num;
+		String grade;
 		
+		Scanner s = new Scanner(System.in);
 		
-		System.out.println("Hello");
-		
+		System.out.print("점수입력: ");
+		num = s.nextInt();
+		grade = Calculator.count(num);
+
+		System.out.println("학점: "+grade);
 	}
+
 }
