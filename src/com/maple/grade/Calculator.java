858db43e5eca3b66869e6c17883c34de649b114a
merryLee <ismpink@daum.net>
2015-01-08 09:15:23 +0900

    수정4

--- src/com/maple/grade/Calculator.java
+++ src/com/maple/grade/Calculator.java
@@ -4,11 +4,11 @@ public class Calculator {
 
 	public static String count(int num) {
 
-		if (num >= 90) return "A";
-		else if (num >= 80) return "B";
-		else if (num >= 70)	return "C";
-		else if (num >= 60)	return "D";
-		else return "F";
+		if (num >= 90) { return "A"; }
+		else if (num >= 80) { return "B"; }
+		else if (num >= 70) { return "C"; }
+		else if (num >= 60) { return "D"; }
+		else { return "F"; }
 	}
 
 	public static int sum(int[] list) {
